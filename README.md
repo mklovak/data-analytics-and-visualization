1fe4713114a6865004e7700bf25db2e86f0a2d1e
Maksym <iimax2208@gmail.com>
2018-11-11 23:40:38 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,38 @@
+
+
+## Task:
+The overall task will involve extracting market data for various trading instruments (represented by unique symbols), applying some transformations and statistical functions, and displaying the results in a report.
+
+1.	Obtain the *QUOTES* series from csv files in folder "data" (files in the repository are zipped) : `TYQUOTES.csv`, `FVQUOTES.csv`
+	The quotes data is stored as a BBO. BBO stands for best bid/offer, which represents the best bid and ask (ask and offer are interchangeable terms) in the market for a given instrument at any given time (timestamps are unix epoch timestmaps in nanoseconds). At each timestamp it contains, in the following order: bid price, bid quantity, ask price, ask quantity
+
+2.	Calculate a `VwMpt` at each timestamp *t* for each instrument's QUOTES series. `VwMpt`is Volume Weighted Average Price, find some info here- https://www.investopedia.com/terms/v/vwap.asp
+	`VwMpt_t = bid_price_t + (ask_price_t-bid_price_t) * (bid_qty_t / (bid_qty_t+ask_qty_t))`
+
+3. Calculate a rolling correlation `rho` of `VwMpt` returns (first difference, i.e. derivative) sampled at 1 minute intervals with a lookback of 100 samples
+
+4. On a webpage, display a plot of `VwMpt` substracted from the first sample (so the time series starts at `0`) of both series on the same line chart, and below it plot the `rho` lined up along the same time axis. You may use a library like [Flot](http://www.flotcharts.org/) or something similar.
+
+## Additional info:
+You will need to know some basics of market data and trading concepts. Also, a basic understanding of time series data. These are good starting points:
+* [Order Books](http://en.wikipedia.org/wiki/Order_book_%28trading%29)
+* [Bid/Ask](https://en.wikipedia.org/wiki/Bid%E2%80%93ask_spread)
+
+## Solution provided:
+1. run script.py
+- it takes two unzipped csv files 'data/FVQUOTES.csv' and 'data/TYQUOTES.csv'
+- calculate a VwMpt at each timestamp t for each instrument's QUOTES series
+- calculate rolling correlation with lookback of 100 samples
+- generate 3 json files: 'data/vwmpt_fv_out_data.json', 'data/vwmpt_ty_out_data.json', 'data/rho_out_data_t.json'
+2. open index.html file to view the report in a requested form
+
+* Actual input data was deleted from a "data" folder because it may be considered as a confidential data. Contact @mklovak to see how provided solution works.
+** In order to run script.py you need to have pandas library installed on your system.
+*** In order to see graph visualisation you need to download, install and link to index.html file jQuery (https://jquery.com/download) and Flot (http://www.flotcharts.org/) libraries.
+
+
+
+
+
+
+
--- script.py
+++ script.py
@@ -1,27 +1,15 @@
 '''
 
-## Task
-The overall task will involve extracting market data for various trading instruments (represented by unique symbols),
-applying some transformations and statistical functions, and displaying the results in a report.
-
-1. Obtain the following *QUOTES* series from csv files: `TYQUOTES.csv`, `FVQUOTES.csv`
-  The quotes data is stored as a BBO. BBO stands for best bid/offer, which represents the best bid and ask
-  (ask and offer are interchangeable terms) in the market for a given instrument at any given time.
-   At each timestamp it contains, in the following order: bid price, bid quantity, ask price, ask quantity
-
-2. Calculate a `VwMpt` at each timestamp *t* for each instrument's QUOTES series
-`VwMpt_t = bid_price_t + (ask_price_t-bid_price_t) * (bid_qty_t / (bid_qty_t+ask_qty_t))`
-
-3. Calculate a rolling correlation `rho` of `VwMpt` returns (first difference, i.e. derivative) sampled
-at 1 minute intervals with a lookback of 100 samples
-
-4. On a webpage, display a plot of `VwMpt` substracted from the first sample (so the time series starts at `0`)
-of both series on the same line chart, and below it plot the `rho` lined up along the same time axis.
-You may use a library like [Flot](http://www.flotcharts.org/) or something similar.
+- to run this script you need to have pandas and json library installed on your system
+- if you want to see plotted data even before sending it to client-side, install matplotlib.pyplot and uncomment related strings (see below)
 
 '''
 
 import pandas as pd
+import ast
+import json
+import matplotlib.pyplot as plt
+
 
 # put here your own path to QUOTES.csv files
 csv_one = 'data/FVQUOTES.csv'
@@ -33,24 +21,28 @@ inst_quotes_two = pd.read_csv(csv_two)
 inst_quotes_one['vwmpt_fv'] = (inst_quotes_one['bestBidPrice'] + (inst_quotes_one['bestOfferPrice']-inst_quotes_one['bestBidPrice']))*(inst_quotes_one['bestBidQty'] / (inst_quotes_one['bestBidQty'] + inst_quotes_one['bestOfferQty']))
 inst_quotes_two['vwmpt_ty'] = (inst_quotes_two['bestBidPrice'] + (inst_quotes_two['bestOfferPrice']-inst_quotes_two['bestBidPrice']))*(inst_quotes_two['bestBidQty'] / (inst_quotes_two['bestBidQty'] + inst_quotes_two['bestOfferQty']))
 
-inst_quotes_one.vwmpt_fv = inst_quotes_one.vwmpt_fv.round(2)
-inst_quotes_two.vwmpt_ty = inst_quotes_two.vwmpt_ty.round(2)
+# round calculated vwmpt id you need that
+# inst_quotes_one.vwmpt_fv = inst_quotes_one.vwmpt_fv.round(2)
+# inst_quotes_two.vwmpt_ty = inst_quotes_two.vwmpt_ty.round(2)
 
 # to prepare data for later plotting we need to merge dataframes and use "nanosec" as common index.
 merged = pd.merge(inst_quotes_one, inst_quotes_two, on = "nanosec", how = 'outer', suffixes=('_fv', '_ty'))
 
-# calculate rolling correlation with lookback of 100 samples
-merged['rho'] = merged['vwmpt_fv'].rolling(window=100).corr(other=merged['vwmpt_ty'])
-
 # delete columns that we dont need anymore
 merged = merged.drop(['bestBidPrice_fv', 'bestBidQty_fv', 'bestOfferPrice_fv', 'bestOfferQty_fv','bestBidPrice_ty','bestBidQty_ty', 'bestOfferPrice_ty', 'bestOfferQty_ty' ], axis=1)
 
-# and convert unix epoch timestmaps in nanoseconds to human-readable form (uncomment, if you need that)
+# calculate rolling correlation with lookback of 100 samples
+merged['rho'] = merged['vwmpt_fv'].rolling(window=100).corr(other=merged['vwmpt_ty'])
+
+# convert unix epoch timestmaps in nanoseconds to human-readable form (used for debugging. uncomment, if you will need that):
 # merged['time'] =  pd.to_datetime(merged['nanosec'], unit='ns')
 # merged.set_index("time", inplace=True)
+
+# convert unix epoch timestmaps in nanoseconds to nix epoch timestmaps in milliseconds.
+# we will need them in milliseconds because it's JSON format expected by Flot jQuery library
 merged['millisec'] = merged['nanosec'].floordiv(1000000)
 
-# For debugging:
+# Used for debugging (uncomment, if you will need that):
 # merged['time_millisec'] =  pd.to_datetime(merged['millisec'], unit='ms')
 # merged.head()
 
@@ -58,35 +50,45 @@ merged['millisec'] = merged['nanosec'].floordiv(1000000)
 merged = merged.dropna()
 merged = merged.reset_index(drop=True)
 
-# Plot the data (uncomment if you need to look at the visualised data):
+# Plot the data (uncomment if you need to look now at the visualised data):
 # merged.plot(y=['vwmpt_fv', 'vwmpt_ty'])
 # merged.plot(y=['rho'])
 # plt.show()
 
-# generate JSON data in an expected by client form. for example: [ [[0, 0], [1, 1]] ]
-subset_rho = merged[['millisec', 'rho']]
+# generate JSON data in an expected by client form (Flot jQuery library). for example:
+''' 
+{ "label": "Europe (EU27)",
+"data": [[1999, 3.0], [2000, 3.9], [2001, 2.0], [2002, 1.2], [2003, 1.3], [2004, 2.5], [2005, 2.0], [2006, 3.1], [2007, 2.9], [2008, 0.9]]
+}
+'''
+subset_vwmpt_fv = merged[['millisec', 'vwmpt_fv']]
+vwmpt_fv_out_data = {
+    "label": "vwmpt_fv",
+    # https://stackoverflow.com/questions/988228/convert-a-string-representation-of-a-dictionary-to-a-dictionary
+    "data": ast.literal_eval(subset_vwmpt_fv.to_json(date_unit = 'ms', orient='values', date_format = 'epoch'))
+}
+with open('data/vwmpt_fv_out_data.json', 'w') as outfile:
+    json.dump(vwmpt_fv_out_data, outfile)
 
-# For debugging:
+
+subset_vwmpt_ty = merged[['millisec', 'vwmpt_ty']]
+vwmpt_ty_out_data = {
+    "label": "vwmpt_ty",
+    "data": ast.literal_eval(subset_vwmpt_ty.to_json(date_unit = 'ms', orient='values', date_format = 'epoch'))
+}
+with open('data/vwmpt_ty_out_data.json', 'w') as outfile:
+    json.dump(vwmpt_ty_out_data, outfile)
+
+
+subset_rho = merged[['millisec', 'rho']]
+rho_out_data = {
+    "label": "rho",
+    "data": ast.literal_eval(subset_rho.to_json(date_unit = 'ms', orient='values', date_format = 'epoch'))
+}
+with open('data/rho_out_data.json', 'w') as outfile:
+    json.dump(rho_out_data, outfile)
+
+# Used for debugging (uncomment, if you will need that):
 # print(subset_rho.head())
 # subset_rho['time_millisec'] =  pd.to_datetime(subset_rho['millisec'], unit='ms')
 # print(subset_rho.head())
-
-rho_out_data = subset_rho.to_json(date_unit = 'ms', orient='values', date_format = 'epoch')
-with open('data/rho_out_data.json', 'w') as outfile:
-    outfile.write(rho_out_data)
-
-subset_vwmpt_fv = merged[['millisec', 'vwmpt_fv']]
-vwmpt_fv_out_data = subset_vwmpt_fv.to_json(date_unit = 'ms', orient='values', date_format = 'epoch')
-with open('data/vwmpt_fv_out_data.json', 'w') as outfile:
-    outfile.write(vwmpt_fv_out_data)
-
-subset_vwmpt_ty = merged[['millisec', 'vwmpt_ty']]
-
-vwmpt_ty1_out_data = subset_vwmpt_ty[:47138].to_json(date_unit = 'ms', orient='values', date_format = 'epoch')
-with open('data/vwmpt_ty1_out_data.json', 'w') as outfile:
-    outfile.write(vwmpt_ty1_out_data)
-
-vwmpt_ty2_out_data = subset_vwmpt_ty[47138:].to_json(date_unit = 'ms', orient='values', date_format = 'epoch')
-with open('data/vwmpt_ty2_out_data.json', 'w') as outfile:
-    outfile.write(vwmpt_ty2_out_data)
-
